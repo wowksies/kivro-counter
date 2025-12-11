c8835d09c7d6020ca9aed9a853ab49862b240880
Wowskies <114474193+wowksies@users.noreply.github.com>
2025-12-11 21:57:14 +0000

    Update _cors.js

--- api/_cors.js
+++ api/_cors.js
@@ -1,12 +1,4 @@
-res.setHeader("Access-Control-Allow-Origin", "https://kivro-games.vercel.app");
-res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-
-if (req.method === "OPTIONS") {
-    return res.status(200).end();
-}
-
-
+// api/_cors.js
 export default function cors(handler) {
   return async (req, res) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
