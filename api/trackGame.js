5945768ed4fc7d8b079eb03da1182a6cd3050a16
Wowskies <114474193+wowksies@users.noreply.github.com>
2025-12-11 21:41:45 +0000

    Implement API handler for tracking game plays

---
+++ api/trackGame.js
@@ -0,0 +1,27 @@
+// api/trackGame.js
+import fs from "fs";
+import path from "path";
+
+export default function handler(req, res) {
+    const { gameId } = req.query;
+
+    if (!gameId) {
+        return res.status(400).json({ error: "Missing gameId" });
+    }
+
+    const file = path.join(process.cwd(), "data.json");
+
+    let data = { visitors: [], games: {} };
+
+    try {
+        data = JSON.parse(fs.readFileSync(file, "utf8"));
+    } catch (err) {}
+
+    if (!data.games[gameId]) data.games[gameId] = 0;
+
+    data.games[gameId] += 1;
+
+    fs.writeFileSync(file, JSON.stringify(data, null, 2));
+
+    res.status(200).json({ gameId, totalPlays: data.games[gameId] });
+}
