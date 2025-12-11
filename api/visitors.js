c6ae1f697e15ded903fd8a148e0cb98ebd06e751
Wowskies <114474193+wowksies@users.noreply.github.com>
2025-12-11 21:18:51 +0000

    Update visitors.js

--- api/visitors.js
+++ api/visitors.js
@@ -1,1 +1,18 @@
+import fs from 'fs';
+import path from 'path';
 
+export default function handler(req, res) {
+  const filePath = path.join(process.cwd(), 'visitors.json');
+
+  if (!fs.existsSync(filePath)) {
+    fs.writeFileSync(filePath, JSON.stringify({ total: 0 }), 'utf8');
+  }
+
+  const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+
+  data.total++;
+
+  fs.writeFileSync(filePath, JSON.stringify(data), 'utf8');
+
+  res.status(200).json({ total: data.total });
+}
