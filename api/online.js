7415a8551a46f2fde0f6c879789b289f5c2153c5
Wowskies <114474193+wowksies@users.noreply.github.com>
2025-12-11 21:19:19 +0000

    Update online.js

--- api/online.js
+++ api/online.js
@@ -1,1 +1,16 @@
+let onlineUsers = 0;
 
+export default function handler(req, res) {
+  if (req.method === "POST") {
+    const { action } = req.body;
+
+    if (action === "join") onlineUsers++;
+    if (action === "leave") onlineUsers--;
+
+    onlineUsers = Math.max(onlineUsers, 0);
+
+    res.status(200).json({ online: onlineUsers });
+  } else {
+    res.status(200).json({ online: onlineUsers });
+  }
+}
